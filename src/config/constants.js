5091045b86f49b187e78aba0af9c0b5a7b9ee30a
water <it_baikaishui@163.com>
2022-01-25 10:13:48 +0800

    chore: 更新配置

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,8 +1,8 @@
 // 模版下载地址
 export const gitConfig = {
     user: 'baikaishui01',
-    repos: 'templatessss'
+    repos: 'templates'
 }
 
 // 初始化工程名
-export const initProjectName = 'init_project'
+export const initProjectName = 'oddl_init_templates'
